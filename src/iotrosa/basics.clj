0e52d52f0e07daff9f8b565d899a87343e9099c5
Clement Van Truyart <me@trosa.io>
2020-12-25 22:53:20 +0100

    Added 303 tests

--- src/iotrosa/basics.clj
+++ src/iotrosa/basics.clj
@@ -1,6 +1,6 @@
 (ns iotrosa.basics
-  (:use [overtone.live]))
-
+  (:use [overtone.live]
+        [overtone.synth.retro]))
 
 ;; basic demos
 (map #(demo 0.4 (sin-osc %))
@@ -14,9 +14,27 @@
 
 (demo (* (env-gen (lin 0.1 1 1 0.25) :action FREE) (sin-osc)))
 
-(demo (let [dur 0.4
+(demo (let [dur 3
             env (sin-osc:kr (/ 1 (* 2 dur)))]
         (line:kr 0 1 dur :action FREE)
         (* env (saw 220))))
+
+(defsynth okay [out-bus 10 amp 0.5]
+  (out out-bus
+       (* amp
+          (+ (* (decay2 (* (impulse 10 0)
+                           (+ (* (lf-saw:kr 0.3 0) -0.3) 0.3))
+                        0.001)
+                0.3)
+             (apply + (pulse [80 81]))))))
+
+(doseq [note (range 20 40 2)]
+  (tb-303 200
+          :gate 2
+          :cutoff 4
+          :note note
+          :amp 50
+          :action FREE
+          :out-bus 1))
 
 (stop-all)
